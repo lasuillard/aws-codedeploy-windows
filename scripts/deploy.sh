29002150f108a6ac2bfa23a002dbff2887f5beaf
Yuchan Lee <lasuillard@gmail.com>
2025-07-09 05:32:18 +0000

    ci: Wait for deployment completion

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -16,9 +16,24 @@ bucket="$3"
 bucket_key="$4"
 echo "Using S3 bucket: ${bucket}, key: ${bucket_key}"
 
-aws deploy create-deployment \
-    --application-name "$application_name" \
-    --deployment-group-name "$deployment_group_name" \
-    --deployment-config-name CodeDeployDefault.AllAtOnce \
-    --s3-location "bucket=${bucket},bundleType=zip,key=${bucket_key}" \
-    --ignore-application-stop-failures
+wait="${5:-"1"}"
+echo "Wait for deployment to complete: ${wait}"
+
+deployment_id="$(
+    aws deploy create-deployment \
+        --application-name "$application_name" \
+        --deployment-group-name "$deployment_group_name" \
+        --deployment-config-name CodeDeployDefault.AllAtOnce \
+        --s3-location "bucket=${bucket},bundleType=zip,key=${bucket_key}" \
+        --ignore-application-stop-failures \
+    | jq -r '.deploymentId'
+)"
+echo "Deployment started with ID: ${deployment_id}"
+
+# Wait for the deployment to succeed
+if [ -n "$wait" ]; then
+    echo "Waiting for deployment to complete..."
+    aws deploy wait deployment-successful --deployment-id "$deployment_id"
+else
+    echo "Skipping wait for deployment completion."
+fi
