d87b7d5a7888ab2f59949d45d7b62261bd414e2a
Yuchan Lee <lasuillard@gmail.com>
2026-08-21 09:16:59 +0000

    Fix dependency issues

--- flake.nix
+++ flake.nix
@@ -24,13 +24,24 @@
             pulumi-bin
             awscli2
             ;
-        inherit (pkgs.pulumiPackages)
-            pulumi-python;
+          inherit (pkgs.pulumiPackages)
+            pulumi-python
+            ;
         };
 
         devShells.default = pkgs.mkShell {
           packages = builtins.attrValues self.packages.${system};
           shellHook = ''
+            export LD_LIBRARY_PATH="${
+              pkgs.lib.makeLibraryPath [
+                pkgs.stdenv.cc.cc.lib
+                pkgs.libxcb
+                pkgs.libX11
+                pkgs.libGL
+                pkgs.glib.out
+              ]
+            }''${LD_LIBRARY_PATH:+:$LD_LIBRARY_PATH}"
+
             pre-commit install
 
             # Workaround for pre-commit leaking its dependencies into the environment
