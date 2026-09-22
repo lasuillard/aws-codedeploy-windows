ec47f5ab63c4cb206dd986b4033379c85fd122c2
Yuchan Lee <lasuillard@gmail.com>
2026-09-22 17:11:29 +0900

    Set window size to 1920x1080

--- src/scrapers/kftc/scraper.py
+++ src/scrapers/kftc/scraper.py
@@ -54,26 +54,38 @@ class KftcScraper:
 
     def _get_webdriver(self) -> Remote:
         options = ChromeOptions()
-        # options.set_capability("platformName", "WINDOWS")
 
         # Disable "... wants to: Access other apps and services on this device" prompt
         # https://peter.sh/experiments/chromium-command-line-switches/#disable-web-security
         options.add_argument("--disable-web-security")
+        options.add_argument("--headless=new")
+        options.add_argument("--no-sandbox")
+        options.add_argument("--disable-dev-shm-usage")
+        options.add_argument("--window-size=1920,1080")
+        options.add_argument("--force-device-scale-factor=1")
 
         if settings.selenium_hub_url is None:
-            options.add_argument("--headless=new")
-            options.add_argument("--no-sandbox")
-            options.add_argument("--disable-dev-shm-usage")
-
             service = Service(log_output="browser.log")
             self._webdriver = Chrome(options=options, service=service)
+
+            # NOTE: New headless mode viewport is restricted by the OS display resolution.
+            #       So we need to explicitly set the device metrics to match the desired viewport size.
+            self._webdriver.execute_cdp_cmd(
+                "Emulation.setDeviceMetricsOverride",
+                {
+                    "width": 1920,
+                    "height": 1080,
+                    "deviceScaleFactor": 1,
+                    "mobile": False,
+                },
+            )
         else:
+            options.set_capability("platformName", "WINDOWS")
             self._webdriver = Remote(
                 command_executor=settings.selenium_hub_url,
                 options=options,
             )
 
-        self._webdriver.maximize_window()
         return self._webdriver
 
     def __enter__(self) -> Self:
