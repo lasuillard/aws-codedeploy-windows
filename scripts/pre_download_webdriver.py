a5e16a9c84b30793964b76ddfd9d3cfdea1ee1e3
Yuchan Lee <lasuillard@gmail.com>
2026-09-22 18:02:28 +0900

    Update pre-download script

--- scripts/pre_download_webdriver.py
+++ scripts/pre_download_webdriver.py
@@ -1,21 +1,41 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.service import Service
+from pathlib import Path
+
+logs_dir = Path("./logs")
+logs_dir.mkdir(parents=True, exist_ok=True)
+dump_dir = Path("./dump")
+dump_dir.mkdir(parents=True, exist_ok=True)
 
 
 def pre_download_webdriver() -> None:
     """Trigger Selenium's internal manager to pre-download the Chrome WebDriver."""
     print("Pre-downloading the Chrome WebDriver...")
     options = webdriver.ChromeOptions()
-    options.browser_version = "stable"
+    options.add_argument("--disable-web-security")
     options.add_argument("--headless=new")
     options.add_argument("--no-sandbox")
     options.add_argument("--disable-dev-shm-usage")
+    options.add_argument("--window-size=1920,1080")
+    options.add_argument("--force-device-scale-factor=1")
 
     print("Running webdriver...")
-    service = Service(log_output="pre_download_webdriver.log")
-    wd = webdriver.Chrome(options=options, service=service)
+    service = Service(log_output=str(logs_dir / "pre_download_webdriver.log"))
     try:
-        wd.get("about:blank")
+        wd = webdriver.Chrome(options=options, service=service)
+        wd.execute_cdp_cmd(
+            "Emulation.setDeviceMetricsOverride",
+            {
+                "width": 1920,
+                "height": 1080,
+                "deviceScaleFactor": 1,
+                "mobile": False,
+            },
+        )
+        wd.get("https://www.google.com")
+        wd.get_screenshot_as_file(
+            str(dump_dir / "pre_download_webdriver_screenshot.png")
+        )
     finally:
         wd.quit()
 
